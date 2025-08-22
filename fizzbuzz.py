09e8a0860012c5a18767424e04eb3e0cf2478cef
YongJae-AI <goldeggduck@gmail.com>
2025-08-22 12:20:35 +0900

    fix: print fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 for j  in range(1+15+1):
-    if j % 3 == 0:
-        print('fizz')
-    elif j % 5 ==0:
-        print('buzz')
-    elif j % 15 == 0:
+    if j % 15 == 0:
         print('fizzbuzz')
+    elif j % 3 ==0:
+        print('fizz')
+    elif j % 5 == 0:
+        print('buzz')
     else:
         print(j)
